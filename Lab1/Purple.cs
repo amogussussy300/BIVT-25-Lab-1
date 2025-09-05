6f75b04bfca0cb294f70675dcdec2d5387b66501
amogussussy300 <141318500+amogussussy300@users.noreply.github.com>
2025-09-05 10:05:08 +0300

    Add files via upload

--- Lab1/Purple.cs
+++ Lab1/Purple.cs
@@ -1,4 +1,5 @@
-﻿namespace Lab1
+﻿using System;
+namespace Lab1
 {
     public class Purple
     {
@@ -7,7 +8,27 @@
             bool answer = false;
 
             // code here
+            bool Task1(int a, int b, int c)
+            {
+                if (a > 0 && b > 0 && c > 0)
+                {
+                    return true;
+                }
+                else if (a < 0 && b < 0 && c < 0)
+                {
+                    return true;
+                }
+                else if (a == 0 && b == 0 & c == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
 
+            answer = Task1(a, b, c);
             // end
 
             return answer;
@@ -17,7 +38,24 @@
             bool answer = false;
 
             // code here
+            bool Task2(int a, int b)
+            {
 
+                if (b != 0 && a % b == 0)
+                {
+                    return true;
+                }
+                else if (a != 0 && b % a == 0) {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            answer = Task2(a, b);
             // end
 
             return answer;
@@ -28,6 +66,25 @@
 
             // code here
 
+
+            bool Task3(int a, int b)
+            {
+
+                if (Math.Pow(a, 2) == b || Math.Pow(b, 2) == a)
+                {
+                    return true;
+                }
+                else if (Math.Pow(a, 3) == b || Math.Pow(b, 3) == a)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            answer = Task3(a, b);
             // end
 
             return answer;
@@ -37,7 +94,15 @@
             double answer = 0;
 
             // code here
+            double Task4(double d, double f, double g)
+            {
+                double x = (Math.Pow(f, 2) - 4 * d * g);
 
+                return x;
+
+            }
+
+            answer = Task4(d, f, g);
             // end
 
             return answer;
@@ -47,7 +112,23 @@
             double answer = 0;
 
             // code here
+            double Task5(double x)
+            {
+                if (x <= -1)
+                {
+                    return 1;
+                }
+                else if (x > -1 && x <= 1)
+                {
+                    return -x;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
 
+            answer = Task5(x);
             // end
 
             return answer;
@@ -57,7 +138,23 @@
             bool answer = false;
 
             // code here
+            bool Task6(double sqar, double circar)
+            {
+                double sqside = Math.Sqrt(sqar);
+                double radius = sqar / 2;
 
+                if (Math.PI * (Math.Pow(radius, 2)) == circar)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            answer = Task6(squareS, circleS);
             // end
 
             return answer;
